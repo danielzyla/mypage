e2e7c53069ecac550469d5806585c198ee477e66
Daniel Żyła <dzmsgbox@gmail.com>
2022-03-31 22:58:59 +0200

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    let sectionOffset;
+    changeOffset();
+    window.addEventListener("resize", changeOffset);
+
+    function changeOffset() {
+        if (window.innerWidth >= 769) {
+            sectionOffset = 50;
+        } else {
+            sectionOffset = 200;
+        }
+    }
+
+    const scrollDown = (section) => {
+        window.scrollTo({
+            top: section.offsetTop - sectionOffset,
+            behavior: 'smooth',
+        });
+    };  
+    
+    var sectionList = document.getElementById("navbar").getElementsByTagName("li");
+
+    for (const sectionLink of sectionList) {
+        const link = sectionLink.querySelector("a");
+        const section = document.getElementById(link.innerText.toLowerCase() + "-section");
+        link.onclick = function() {
+            scrollDown(section);
+            console.log(section.offsetTop);
+        }
+    }
+}
